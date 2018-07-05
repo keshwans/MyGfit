665bf627200ac2052ec5392e2efac957977a1d52
Sultan Keshwani <sultan.keshwani@gmail.com>
2018-07-05 18:16:18 -0400

    retrieve daily/weekly step counts

--- app/src/main/java/com/newtech/mygfit/MainActivity.java
+++ app/src/main/java/com/newtech/mygfit/MainActivity.java
@@ -1,18 +1,46 @@
 package com.newtech.mygfit;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.Bucket;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSet;
 import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.request.DataReadRequest;
+import com.google.android.gms.fitness.result.DataReadResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 1001;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private TextView mStepsToday;
+    private TextView mStepsWeekly;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +48,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mStepsToday = findViewById(R.id.steps_today);
+        mStepsWeekly = findViewById(R.id.steps_week);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -30,6 +61,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        initializeGoogleFit();
+    }
+
+    private void initializeGoogleFit() {
+        FitnessOptions fitnessOptions = FitnessOptions.builder()
+                .addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+                .addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
+                .build();
+
+        if (!GoogleSignIn.hasPermissions(GoogleSignIn.getLastSignedInAccount(this), fitnessOptions)) {
+            GoogleSignIn.requestPermissions(
+                    this, // your activity
+                    GOOGLE_FIT_PERMISSIONS_REQUEST_CODE,
+                    GoogleSignIn.getLastSignedInAccount(this),
+                    fitnessOptions);
+        } else {
+            readStepsCountForToday();
+            readStepsCountForWeek();
+        }
     }
 
     @Override
@@ -52,5 +102,231 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == GOOGLE_FIT_PERMISSIONS_REQUEST_CODE) {
+                readStepsCountForToday();
+                readStepsCountForWeek();
+            }
+        }
+    }
+
+    private void readStepsCountForToday() {
+
+        Calendar cal = Calendar.getInstance();
+        Date now = new Date();
+        cal.setTime(now);
+
+        long endTime = cal.getTimeInMillis();
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        long startTime = cal.getTimeInMillis();
+
+        final DataReadRequest readRequest = new DataReadRequest.Builder()
+                .read(DataType.TYPE_STEP_COUNT_DELTA)
+                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+                .build();
+
+//        DataReadResult dataReadResult =
+//                Fitness.HistoryApi.readData(mGoogleApiFitnessClient, readRequest).await(1, TimeUnit.MINUTES);
+
+        Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .readData(readRequest)
+                .addOnSuccessListener(new OnSuccessListener() {
+                    @Override
+                    public void onSuccess(Object o) {
+                        Log.d(LOG_TAG, "onSuccess()");
+                        if (o instanceof DataReadRequest) {
+                            DataReadResponse dataReadResponse = (DataReadResponse) o;
+                            DataSet stepData = dataReadResponse.getDataSet(DataType.TYPE_STEP_COUNT_DELTA);
+
+                            int totalSteps = 0;
+
+                            for (DataPoint dp : stepData.getDataPoints()) {
+                                for (Field field : dp.getDataType().getFields()) {
+                                    int steps = dp.getValue(field).asInt();
+
+                                    totalSteps += steps;
+
+                                }
+                            }
+                        }
+                    }
+
+                    public void onSuccess(DataReadResponse dataReadResponse) {
+                        Log.d(LOG_TAG, "onSuccess()");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(LOG_TAG, "onFailure()", e);
+                    }
+                })
+                .addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        Log.d(LOG_TAG, "onComplete()");
+
+                        DataReadResponse dataReadResponse = (DataReadResponse) task.getResult();
+                        DataSet stepData = dataReadResponse.getDataSet(DataType.TYPE_STEP_COUNT_DELTA);
+
+                        int totalSteps = 0;
+
+                        for (DataPoint dp : stepData.getDataPoints()) {
+                            for (Field field : dp.getDataType().getFields()) {
+                                int steps = dp.getValue(field).asInt();
+                                totalSteps += steps;
+                            }
+                        }
+                        Log.i(LOG_TAG, "total steps for this week: " + totalSteps);
+                        mStepsToday.setText("Total Steps today:" + totalSteps);
+                    }
+                });
+    }
+
+    private void readStepsCountForWeek1() {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        long endTime = cal.getTimeInMillis();
+        cal.add(Calendar.WEEK_OF_YEAR, -1);
+        long startTime = cal.getTimeInMillis();
+
+        DataReadRequest readRequest = new DataReadRequest.Builder()
+                .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
+                .bucketByTime(1, TimeUnit.DAYS)
+                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+                .build();
+
+//        DataReadResult dataReadResult =
+//                Fitness.HistoryApi.readData(mGoogleApiFitnessClient, readRequest).await(1, TimeUnit.MINUTES);
+
+        Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .readData(readRequest)
+                .addOnSuccessListener(new OnSuccessListener() {
+                    @Override
+                    public void onSuccess(Object o) {
+                        Log.d(LOG_TAG, "onSuccess()");
+                        if (o instanceof DataReadRequest) {
+                            DataReadResponse dataReadResponse = (DataReadResponse) o;
+                            DataSet stepData = dataReadResponse.getDataSet(DataType.AGGREGATE_STEP_COUNT_DELTA);
+
+                            int totalSteps = 0;
+
+                            for (DataPoint dp : stepData.getDataPoints()) {
+                                for (Field field : dp.getDataType().getFields()) {
+                                    int steps = dp.getValue(field).asInt();
+                                    totalSteps += steps;
+                                }
+                            }
+                        }
+                    }
+
+                    public void onSuccess(DataReadResponse dataReadResponse) {
+                        Log.d(LOG_TAG, "onSuccess()");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(LOG_TAG, "onFailure()", e);
+                    }
+                })
+                .addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        Log.d(LOG_TAG, "onComplete()");
+
+                        DataReadResponse dataReadResponse = (DataReadResponse) task.getResult();
+                        DataSet stepData = dataReadResponse.getDataSet(DataType.AGGREGATE_STEP_COUNT_DELTA);
+
+                        int totalSteps = 0;
+
+                        for (DataPoint dp : stepData.getDataPoints()) {
+                            for (Field field : dp.getDataType().getFields()) {
+                                int steps = dp.getValue(field).asInt();
+                                totalSteps += steps;
+                            }
+                        }
+                        Log.i(LOG_TAG, "total steps for this week: " + totalSteps);
+                        mStepsWeekly.setText("Total steps for week: " + totalSteps);
+                    }
+                });
+    }
+
+    private void readStepsCountForWeek() {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        long endTime = cal.getTimeInMillis();
+        cal.add(Calendar.WEEK_OF_YEAR, -1);
+        long startTime = cal.getTimeInMillis();
+
+        DataReadRequest readRequest = new DataReadRequest.Builder()
+                .aggregate(DataType.TYPE_STEP_COUNT_DELTA, DataType.AGGREGATE_STEP_COUNT_DELTA)
+                .bucketByTime(1, TimeUnit.DAYS)
+                .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+                .build();
+
+        Fitness.getHistoryClient(this, GoogleSignIn.getLastSignedInAccount(this)).readData(readRequest)
+                .addOnSuccessListener(new OnSuccessListener<DataReadResponse>() {
+                    @Override
+                    public void onSuccess(DataReadResponse dataReadResponse) {
+                        List<Bucket> buckets = dataReadResponse.getBuckets();
+
+                        int totalSteps = 0;
+
+                        for (Bucket bucket : buckets) {
+
+                            List<DataSet> dataSets = bucket.getDataSets();
+
+                            for (DataSet dataSet : dataSets) {
+//                                if (dataSet.getDataType() == DataType.TYPE_STEP_COUNT_DELTA) {
+
+
+                                for (DataPoint dp : dataSet.getDataPoints()) {
+                                    for (Field field : dp.getDataType().getFields()) {
+                                        int steps = dp.getValue(field).asInt();
+                                        totalSteps += steps;
+                                    }
+                                }
+                            }
+//                            }
+                        }
+                        Log.i(LOG_TAG, "total steps for this week: " + totalSteps);
+                        mStepsWeekly.setText("Total steps for week: " + totalSteps);
+                    }
+                })
+                .addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        List<Bucket> buckets = ((DataReadResponse) task.getResult()).getBuckets();
+
+                        int totalSteps = 0;
+
+                        for (Bucket bucket : buckets) {
+
+                            List<DataSet> dataSets = bucket.getDataSets();
+
+                            for (DataSet dataSet : dataSets) {
+//                                if (dataSet.getDataType() == DataType.TYPE_STEP_COUNT_DELTA) {
+
+
+                                for (DataPoint dp : dataSet.getDataPoints()) {
+                                    for (Field field : dp.getDataType().getFields()) {
+                                        int steps = dp.getValue(field).asInt();
+                                        totalSteps += steps;
+                                    }
+                                }
+                            }
+//                            }
+                        }
+                        Log.i(LOG_TAG, "total steps for this week: " + totalSteps);
+                        mStepsWeekly.setText("Total steps for week: " + totalSteps);
+                    }
+                });
     }
 }
